901955b14dfdf09031c6b9a22406fab26dd9a08b
klockett <kelockett@fullsail.edu>
2015-01-13 19:56:18 -0800

    Expression Worksheet
    sparky age in do years also in human years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+// Ken Lockett
+// 01/13/2015
+// Expressions Worksheet
+
+//Dog Years
+var sparkysHumanAge = 2;
+sparkysHumanAge = sparkysHumanAge * 7;
+ console.log(sparkysHumanAge)
---
+++ Lockett_Ken_Expressions_Assignment./js/script.js
@@ -0,0 +1,5 @@
+//Expressions - Age Example
+
+var yearBorn = 1976;
+var age = 2015 - yearBorn
+console.log(age);
