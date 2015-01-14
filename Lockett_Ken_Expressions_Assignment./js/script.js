8f47f30220819a81b659316dd1342e19b3cc04df
klockett <kelockett@fullsail.edu>
2015-01-14 12:11:04 -0800

    Product Arrays
    I added the product arrays with style of shoes and cost.

--- Lockett_Ken_Expressions_Assignment./js/script.js
+++ Lockett_Ken_Expressions_Assignment./js/script.js
@@ -3,13 +3,16 @@
 // Price for Air Jordan shoes plus tax if applicable.
 // Givens below
 
-var airJordanShoeCost = 200; //Given-Shoe cost $200.00
-var shipCost = 3;             //Given- Ship Cost $3.00
-var tax = 1.09;               //Given-tax Cost = 1.09
-var totalCost;                //Given total Cost
+var airJordanShoeCost = 200; // Given-Shoe cost $200.00
+var shipCost = 3;             // Given- Ship Cost $3.00
+var tax = 1.09;               // Given-tax Cost = 1.09
+var totalCost;                // Given total Cost
 
 var totalCost = airJordanShoeCost * tax + shipCost  // shoe cost  was multiplied by tax cost plus ship cost
-console.log(totalCost)        //console.log was ran and total came to $221.00
+                                                   // 2-Arithmetic operators used in above variable
+//console.log(totalCost)        //console.log was ran and total came to $221.00
 
-//var airJordanStylesAndCost = ["Air JordanRetro 1 $200", "Air JordanRetro 4 $200", "Air Jordan Retro 11 $200"];
+// Product Arrays with Product Styles and cost
+var airJordanStylesAndCost = ["Air JordanRetro 1 $200", "Air JordanRetro 4 $200", "Air Jordan Retro 11 $200"];
+
 //console.log(airJordanStylesAndCost)
