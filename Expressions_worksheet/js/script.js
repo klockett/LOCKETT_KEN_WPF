b7c2c1e90ac80627285deed446318c921f2c32f6
klockett <kelockett@fullsail.edu>
2015-01-13 20:26:57 -0800

    Slice of pie part 1
    Slices per person

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,6 +3,14 @@
 // Expressions Worksheet
 
 //Dog Years
-var sparkysHumanAge = 2;
-sparkysHumanAge = sparkysHumanAge * 7;
- console.log(sparkysHumanAge)
+//var sparkysHumanAge = 2;
+//sparkysHumanAge = sparkysHumanAge * 7;
+ //console.log(sparkysHumanAge)
+
+//Slice of pie part 1
+var slicesPerPizza = 8;
+var peopleAtTheParty = 15;
+var pizzasOrdered = 8;
+
+var slicesPerPerson = pizzasOrdered * slicesPerPizza/peopleAtTheParty;
+console.log(slicesPerPerson);
