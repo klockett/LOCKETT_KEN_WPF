e73c828fe9313fd05d42596e4509d4fd52b18a50
klockett <kelockett@fullsail.edu>
2015-01-14 13:38:02 -0800

    String variable for output
    I added the string variable  for out also tested and ran successful.

--- Lockett_Ken_Expressions_Assignment./js/script.js
+++ Lockett_Ken_Expressions_Assignment./js/script.js
@@ -8,11 +8,18 @@ var shipCost = 3;             // Given- Ship Cost $3.00
 var tax = 1.09;               // Given-tax Cost = 1.09
 var totalCost;                // Given total Cost
 
-var totalCost = airJordanShoeCost * tax + shipCost  // shoe cost  was multiplied by tax cost plus ship cost
+var totalCost = airJordanShoeCost * tax + shipCost;  // shoe cost  was multiplied by tax cost plus ship cost
                                                    // 2-Arithmetic operators used in above variable
-//console.log(totalCost)        //console.log was ran and total came to $221.00
+//console.log(totalCost);        //console.log was ran and total came to $221.00
 
-// Product Arrays with Product Styles and cost
-var airJordanStylesAndCost = ["Air JordanRetro 1 $200", "Air JordanRetro 4 $200", "Air Jordan Retro 11 $200"];
+//Product Arrays with Product Styles and cost
+var airJordanStylesAndCost = ["Air JordanRetro 1 $200", "Air JordanRetro 4 $200","Air Jordan Retro 11 $200"];
 
-//console.log(airJordanStylesAndCost)            // Console.log was ran for arrays and each item was listed in test
+//console.log(airJordanStylesAndCost);            // Console.log was ran for arrays and each item was listed in test
+
+// A String  variable for output
+var airJordanShoeCost= prompt("Please input the cost of your shoes 200");
+var tax= prompt("Please input the \(State Tax rate of 1.09\).\n A $3.00 Shipping charge also will be added")
+var totalCost = airJordanShoeCost * tax + shipCost;
+
+console.log(totalCost);
