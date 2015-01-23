3e3a69935901ce64e067fab1a8706abae520a0d7
klockett <kelockett@fullsail.edu>
2015-01-22 21:50:13 -0800

    2 Different Arithmetic Operators
    I Add 2 different Arithmetic Operators

--- Lockett_Ken_Conditionals_Assignment/js/script.js
+++ Lockett_Ken_Conditionals_Assignment/js/script.js
@@ -2,11 +2,20 @@
   SDI- Section-3
   01/18/2015
  */
-var cheesePizza="5";
-var pepperoniPizza="10";
+//Given variables listed
+var cheesePizza=5;
+var pepperoniPizza=10;
 var tip;
 var deliveryFee="3";
 var totalAmount;
+
+//2 Different  Arithmetic Operators
+
+totalAmount= deliveryFee - deliveryFee + cheesePizza + pepperoniPizza
+console.log(totalAmount)
+
+
+
 
 
 
