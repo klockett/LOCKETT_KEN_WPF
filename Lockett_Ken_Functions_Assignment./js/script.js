e08c84c3f1e3202e70d5d566896f328916e967a1
klockett <kelockett@fullsail.edu>
2015-01-26 22:58:59 -0800

    Normal function
    1 normal function added including argument and 3 parameters with a
    value returned also passed were used

--- Lockett_Ken_Functions_Assignment./js/script.js
+++ Lockett_Ken_Functions_Assignment./js/script.js
@@ -1,3 +1,18 @@
-/**
- * Created by kenlockett1 on 1/26/15.
+/*
+ Ken lockett
+ SDI -Section 3
+ Functions Assignment
+ 1/26/15
  */
+var total = totalIceCreamScoops(1, 1, 2);
+totalIceCreamScoops(1,1,2); //arguments used
+function totalIceCreamScoops (iceCreamScoops, whipCream, waffleCone) { //3 parameters used
+    var totalIceCreamScoops = iceCreamScoops + 1 + 2
+    console.log("Your total of Ice Cream scoops is " + iceCreamScoops + " plus a $" + whipCream +
+    " of Whip Cream and a $" + waffleCone + " Waffle Cone.");
+
+  return totalIceCreamScoops;//return
+}
+totalIceCreamScoops(4, 1, 2);// pass used
+totalIceCreamScoops(7, 1, 2);//pass ussed
+console.log(total);
