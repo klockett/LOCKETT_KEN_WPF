e848af5139acc1787f3c66aa999bf2e29a8f5fed
klockett <kelockett@fullsail.edu>
2015-01-27 22:04:23 -0800

    Loops with validated Prompts
    I added Loops and validated Prompts

--- Lockett_Ken_Functions_Assignment./js/script.js
+++ Lockett_Ken_Functions_Assignment./js/script.js
@@ -4,6 +4,8 @@
  Functions Assignment
  1/26/15
  */
+
+//1 Normal function
 var total = totalIceCreamScoops(1, 1, 2);
 totalIceCreamScoops(1,1,2); //arguments used
 function totalIceCreamScoops (iceCreamScoops, whipCream, waffleCone) { //3 parameters used
@@ -14,15 +16,60 @@ function totalIceCreamScoops (iceCreamScoops, whipCream, waffleCone) { //3 param
   return totalIceCreamScoops;//return
 }
 totalIceCreamScoops(4, 1, 2);// pass used
-totalIceCreamScoops(7, 1, 2);//pass ussed
+totalIceCreamScoops(7, 1, 2);//pass used
 console.log(total);
 
 //1 Anonymous Function
-    var totalIceCreamScoops = function(iceCreamScoops, whipCream, waffleCone) {
+    var totalIceCreamScoops = function(iceCreamScoops, whipCream, waffleCone) {// 3 parameters
 
         var total = iceCreamScoops + whipCream + waffleCone
     return total;
 }
-    var a = totalIceCreamScoops(1, 1, 2);
+    var a = totalIceCreamScoops(1, 1, 2); // arguments
 
      console.log(a);
+
+ var iceCreamScoops = 4;
+
+var iceCreamScoops = prompt("Please put how many scoops of Ice Cream you would like!")
+
+if(iceCreamScoops == ""){
+         console.log("Left it Blank");
+ iceCreamScoops = prompt("Please dont leave blank How many scoops would you like?");
+}
+ while (iceCreamScoops > 0){
+     //console.log("left it blank")
+     console.log("You have " + iceCreamScoops + " Scoops of Ice Cream");
+     iceCreamScoops--;
+
+ }
+
+var whipCream = 4;
+
+var whipCream = prompt("Please put how many Sprays of Whip Cream you would like!")
+
+if(whipCream == ""){
+    console.log("Left it Blank");
+    whipCream = prompt("Please dont leave blank How many Sprays would you like?");
+}
+while (whipCream > 0) {
+    //console.log("left it blank")
+    console.log("You have " + whipCream + " Gentle Sprays of Whip Cream");
+
+    whipCream--;
+}
+
+var cherries = 4;
+
+var cherries = prompt("Please put how many cherries would like on top!")
+
+if(cherries == ""){
+    console.log("Left it Blank");
+    cherries = prompt("Please dont leave blank How many Cherries would you like?");
+}
+while (cherries > 0) {
+    //console.log("left it blank")
+    console.log("You have " + cherries + " Cherries on top!");
+
+    cherries--;
+}
