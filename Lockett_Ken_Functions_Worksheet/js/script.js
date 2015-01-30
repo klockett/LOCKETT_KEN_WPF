7150ba9ab63ed2ebb8fda21c90d5fccede23e506
klockett <kelockett@fullsail.edu>
2015-01-29 20:44:22 -0800

    Function Worksheet
    Total bee stings it take to kill a animal by pound

--- Lockett_Ken_Functions_Worksheet/js/script.js
+++ Lockett_Ken_Functions_Worksheet/js/script.js
@@ -6,3 +6,12 @@ Functions Worksheet
  */
 
 //alert("test");
+
+beeStings (8.7,1);
+
+function beeStings(beeStings,pound){
+    var totalBeeStings = beeStings * pound
+    console.log("It takes a "totalBeeStings "to kill this animal.")
+}
+
+//Started to late to finish
